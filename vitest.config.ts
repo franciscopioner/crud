413bf8615e7984ed30d88ce095dad88276a8d5d6
Francisco <francisco.pioner@gmail.com>
2022-10-18 11:10:17 -0300

    chore: add vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,22 +2,23 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
-        globals: true,
-        environment: 'jsdom',
-        coverage: {
-          provider: 'istanbul', // or 'c8',
-          all: true,
-          reporter: ['lcov', 'html', 'text'],
-          reportsDirectory: 'coverage',
-          include: ['src/**'],
-          exclude: [
-            'src/**/*.d.ts',
-            'src/test/**',
-            'src/**/*.{spec, test}.{ts.tsx}',
-            'src/styles/**',
-            'src/constants/**'
-          ],
-        },
-        include: ['**/src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      setupFiles: './src/test/setup.ts',
+      globals: true,
+      environment: 'jsdom',
+      coverage: {
+        provider: 'istanbul', // or 'c8',
+        all: true,
+        reporter: ['lcov', 'html', 'text'],
+        reportsDirectory: 'coverage',
+        include: ['src/**'],
+        exclude: [
+          'src/**/*.d.ts',
+          'src/test/**',
+          'src/**/*.{spec, test}.{ts.tsx}',
+          'src/styles/**',
+          'src/constants/**'
+        ],
+      },
+      include: ['**/src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     },
 });
